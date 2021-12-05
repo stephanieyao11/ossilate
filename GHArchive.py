1f36211ddd43210d12b5306fb688640cba64c453
stephanieyao11 <56286875+stephanieyao11@users.noreply.github.com>
2021-12-05 14:39:59 -0500

    Add files via upload

--- GHArchive.py
+++ GHArchive.py
@@ -1,7 +1,32 @@
 import gzip
 import json
+import ast
 
-with gzip.open("2021-11-25-15.json.gz") as f:
-	data = f.read()
-	j = json.loads(data.decode('utf-8'))
-	print(type(j))
+# with gzip.open("2021-11-25-15.json.gz") as f:
+# 	data = f.read()
+# 	j = json.loads(data.decode('utf-8'))
+# 	print(j[0])
+
+
+
+# f = open('2021-11-25-15.json')
+# data = json.load(f)
+# for i in data:
+#     print(i)
+
+# f.close()
+
+tweets = []
+
+for line in open('2021-11-25-15.json', 'r'):
+	# print(ast.literal_eval(line))
+	# print(json.loads(line))
+	if "PullRequestReviewEvent" in line:
+		print(line)
+	# print(type(line))
+	# break
+#     tweets.append(json.loads(line))
+#     break
+
+# print(tweets)
+
